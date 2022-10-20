6562390f80dc66abbe3675607af6273ad4f1975b
denisuv <devarhanov@gmail.com>
2022-10-20 23:17:27 +0300

    release beta version-0.1.1

---
+++ src/components/Navbar/navData.js
@@ -0,0 +1,32 @@
+export const navLinks = [
+  {
+    label: 'Projects',
+    pathname: '/#project-1',
+  },
+  {
+    label: 'Details',
+    pathname: '/#details',
+  }, 
+  {
+    label: 'Contact',
+    pathname: '/contact',
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/devarhanov',
+    icon: 'twitter',
+  },
+  {
+    label: 'Figma',
+    url: 'https://www.figma.com/@devarhanov',
+    icon: 'figma',
+  },
+  {
+    label: 'Github',
+    url: 'https://github.com/denisuv',
+    icon: 'github',
+  },
+];
